da300047525e4940f5f4e45bf1c12aff18f02185
shreyaparadkar <srparadkar15@gmail.com>
2020-09-10 20:36:22 +0530

    index and game pages added

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask,render_template,url_for
+import requests
+
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    games=['pong','donkeykong']
+    return render_template('index.html',games=games)
+
+@app.route('/game/pong')
+def pingpong():
+    data={
+        'name':'Ping Pong',
+        'desc':'Pong is a table tennis-themed arcade video game featuring simple two-dimensional graphics, manufactured by Atari and originally released in 1972.'
+    }
+    return render_template('game.html',data=data)
 
 if __name__=='__main__':
     app.run(debug=True)
